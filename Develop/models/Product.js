7583e95c143e21faa573364da9a869b8f0602525
unknown <steinway.nathan@gmail.com>
2022-02-19 18:04:01 -0600

    models are constructed

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    
+    produce_price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    product_stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+     category_id: {
+       type: DataTypes.INTEGER,
+       references: {
+         model: 'category',
+         key: 'id'
+       }
+     }
   },
   {
     sequelize,
