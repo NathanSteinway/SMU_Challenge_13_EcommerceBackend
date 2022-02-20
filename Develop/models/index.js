b7b1ec50fbb5b1543943b7f45c240cd1823e7ccf
unknown <steinway.nathan@gmail.com>
2022-02-19 19:09:59 -0600

    completed associations for now

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Category can have many products.
 
-// Categories have many Products
+Category.HasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+// Products can only have one category.
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
